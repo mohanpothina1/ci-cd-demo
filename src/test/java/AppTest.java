3300a03d5c9346198d7eb563bf072ba54711c689
mohanpothina1 <pothinamohan35@gmail.com>
2025-07-29 16:39:36 +0530

    Test commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+
+
+import com.example.App;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3));
+    }
+}
