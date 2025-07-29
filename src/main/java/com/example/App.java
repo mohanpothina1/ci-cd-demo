94bdded836bdc08170c1006630952d43ce759d35
mohanpothina1 <pothinamohan35@gmail.com>
2025-07-29 16:37:42 +0530

    First working commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from CI/CD Pipeline!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
